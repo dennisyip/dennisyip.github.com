075d535fc880a614f14fdeab1842dd8745802c2d
dennisyip <dennis.yip1@gmail.com>
2015-08-05 13:58:05 -0400

    beginning of angular store app

---
+++ store-app/app.js
@@ -0,0 +1,13 @@
+(function(){
+	var app = angular.module('gemStore',[]);
+
+	app.controller('StoreController', function(){
+		this.product = gem;
+	});
+
+	var gem = {
+		name: 'Dodecahedron',
+		price: 2.95,
+		description: 'A beautiful gem',
+	}
+})();
