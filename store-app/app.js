e795fa9ddd87b6e208cbe72cda393f3e231ee198
dennisyip <d888yip@gmail.com>
2015-08-07 14:29:21 -0400

    tabs for gems added
    :wq

--- store-app/app.js
+++ store-app/app.js
@@ -35,5 +35,16 @@
 		}
 
 	];
+
+	app.controller("PanelController", function(){
+		this.tab = 1;
+
+		this.selectTab = function(setTab){
+			this.tab = setTab;
+		};
+		this.isSelected = function(checkTab){
+			return this.tab === checkTab;
+		};
+	});
 		
 })();
