81250ce2813bba7b9df8e7b552ec0f3ebb2f31ba
Dennis Yip <d888yip@gmail.com>
2022-08-29 10:10:51 -0400

    js logic and splitting multiple components

--- react-app/src/components/ExpenseItem.js
+++ react-app/src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>August 28th 2022</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$800.99</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
