aa306631e3f4a3189747bbffd279126d30bb094d
Dennis Yip <d888yip@gmail.com>
2022-08-30 08:58:54 -0400

    organize components, import react

--- react-app/src/components/Expenses/ExpenseItem.js
+++ react-app/src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,9 @@
+import React from "react";
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card";
+import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -12,6 +13,6 @@ function ExpenseItem(props) {
       </div>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
